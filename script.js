f06315dd85b498884e367c1193f61b8e04feb363
gabriel-peixoto <89696365+gabriel-peixoto@users.noreply.github.com>
2023-06-19 22:30:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function input (num){
+/*function input (num){
     var numero = document.getElementById("resultado").innerHTML;
     document.getElementById("resultado").innerHTML = numero + num;
 }
@@ -15,10 +15,53 @@ function apagar (){
 function calcular(){
     var resultado = document.getElementById("resultado").innerHTML;
     if(resultado != ""){
-        document.getElementById("resultado").innerHTML = eval(resultado);
+        document.getElementById("resultado") = eval(resultado);
     }
     else{
-        document.getElementById("resultado").innerHTML = "Erro"
+        document.getElementById("resultado").innerHTML = "0"
     }
 
+}*/
+
+var resultado = document.getElementById("resultado");
+
+function adicionarNumero(numero) {
+  resultado.value += numero;
 }
+
+function adicionarOperador(operador) {
+  resultado.value += operador;
+}
+
+function calcular() {
+  try {
+    resultado.value = eval(resultado.value);
+  } catch (error) {
+    resultado.value = "Erro";
+  }
+}
+
+function limpar() {
+  resultado.value = "";
+}
+
+function apagarUltimoCaractere() {
+    resultado.value = resultado.value.slice(0, -1);
+  }
+
+
+
+    document.addEventListener("keydown", function(event) {
+        var key = event.key;
+  
+        if (key === "Enter") {
+          event.preventDefault();
+          calcular();
+        } else if (key === "Backspace") {
+            apagarUltimoCaractere();
+        } else if (/[0-9+\-*/.=]/.test(key)) {
+          resultado.value += key;
+        } else if (key === "Escape") {
+          limpar();
+        }
+      });
